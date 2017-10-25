3e158f3efb7877fec607bf41193d2864ed87388d
Manpreet <mpsingh17@outlook.co.nz>
2017-10-26 10:57:29 +1300

    Rendering clients in client component from firebase backend.

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,13 +9,23 @@ import { Client } from '../../models/Client';
 })
 export class ClientsComponent implements OnInit {
   clients: any[];
+  totalOwed: number;
 
   constructor(private clientService: ClientService) {}
 
   ngOnInit() {
     this.clientService.getClients().subscribe(clients => {
-      console.log(clients);
+      // console.log(clients);
+      this.clients = clients;
+      this.getTotalOwed();
     });
   }
 
+  getTotalOwed() {
+    let total = 0;
+    for (const client of this.clients) {
+      total += parseFloat(client.balance);
+    }
+    this.totalOwed = total;
+  }
 }
