90e6bbdfd714988d509c5345eef1a2622d81eb05
Manpreet <mpsingh17@outlook.co.nz>
2017-10-31 07:11:56 +1300

    Login form created. Calling service in its TS file.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessages: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authService.login(this.email, this.password)
+    .then((res) => {
+      this.flashMessages.show('You are logged in.', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigateByUrl('/');
+    })
+    .catch((err) => {
+      this.flashMessages.show(err.message, {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+      this.router.navigateByUrl('/login');
+    });
   }
 
 }
