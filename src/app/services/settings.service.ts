f185d44dcc4f324ea933c39c4e0e687c8701b3f5
Manpreet <mpsingh17@outlook.co.nz>
2017-11-02 08:07:39 +1300

    Add settings page and its implementation.

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -4,15 +4,22 @@ import { Settings } from '../models/Settings';
 @Injectable()
 export class SettingsService {
   settings: Settings = {
-    allowRegistration: false,
+    allowRegistration: true,
     disableBalanceOnAdd: true,
     disableBalanceOnEdit: true,
   };
 
-  constructor() { }
+  constructor() {
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings() {
     return this.settings;
   }
 
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
+  }
 }
